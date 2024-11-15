2924e32896797af3cee061206a7f68f4ba6479bc
Lorenzo Guetta <lorenzoguetta@gmail.com>
2024-11-15 22:06:41 +0100

    Organization of repo

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/__init__.py
-
-from .data_preprocessing import load_price_data, load_and_clean_tweets
-from .sentiment_analysis import perform_sentiment_analysis
-from .feature_engineering import create_features
-from .model_training import train_model
